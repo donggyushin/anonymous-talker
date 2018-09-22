dd1598201724e7246356d510b536bec5193dc1ff
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-09-22 15:30:15 +0900

    no changes

--- server/build/main.js
+++ server/build/main.js
@@ -15,13 +15,28 @@ var _index2 = _interopRequireDefault(_index);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 var port = 8080;
 
 app.use(_bodyParser2.default.json());
+app.use("/", _express2.default.static(__dirname + "/../../client/build"));
 
 app.use("/api", _index2.default);
 
-app.listen(port, function () {
+io.on("connection", function (socket) {
+  console.log(socket.id + " has logged in");
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected: ", socket.id);
+  });
+
+  socket.on("send message", function (displayName, message) {
+    io.emit("receive message", displayName, message);
+  });
+});
+
+http.listen(port, function () {
   console.log("Express is listening on port ", port);
 });
